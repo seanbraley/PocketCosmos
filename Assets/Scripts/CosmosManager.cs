77087b1486ef710aa306c92c94854e4d15007add
Susie <hwang.sus@gmail.com>
2016-01-25 19:07:50 -0500

    Starting work on procgen cosmos
    Starting work on procedural generation of the cosmos. Using Unity's
    tutorial (
    https://unity3d.com/learn/tutorials/projects/2d-roguelike-tutorial ) for
    a basis on placement...
    Currently looking at trying to reuse Liam's galaxy code as each "tile"
    in the cosmos grid.

--- Assets/Scripts/CosmosManager.cs
+++ Assets/Scripts/CosmosManager.cs
@@ -31,18 +31,12 @@ namespace Completed
 		}
 
 
-		public int columns = 8;                                         //Number of columns in our game board.
-		public int rows = 8;                                            //Number of rows in our game board.
-		public Count wallCount = new Count (5, 9);                      //Lower and upper limit for our random number of walls per level.
-		public Count foodCount = new Count (1, 5);                      //Lower and upper limit for our random number of food items per level.
-		public GameObject exit;                                         //Prefab to spawn for exit.
-		public GameObject[] floorTiles;                                 //Array of floor prefabs.
-		public GameObject[] wallTiles;                                  //Array of wall prefabs.
-		public GameObject[] foodTiles;                                  //Array of food prefabs.
-		public GameObject[] enemyTiles;                                 //Array of enemy prefabs.
-		public GameObject[] outerWallTiles;                             //Array of outer tile prefabs.
+		public int columns = 8;                                         //Number of columns in cosmos board.
+		public int rows = 8;                                            //Number of rows in cosmos board.
+		public GameObject[] systemTiles;                                //Array of galaxy prefabs.
+        public ulong randSeed;                                          //64-bit unsigned integer seed for RNG
 
-		private Transform boardHolder;                                  //A variable to store a reference to the transform of our Board object.
+        private Transform cosmosHolder;                                  //A variable to store a reference to the transform of Cosmos object.
 		private List <Vector3> gridPositions = new List <Vector3> ();   //A list of possible locations to place tiles.
 
 
@@ -65,11 +59,11 @@ namespace Completed
 		}
 
 
-		//Sets up the outer walls and floor (background) of the game board.
-		void BoardSetup ()
+		//Sets up the outer walls and floor (background) of the space board.
+		void CosmosSetup ()
 		{
-			//Instantiate Board and set boardHolder to its transform.
-			boardHolder = new GameObject ("Board").transform;
+            //Instantiate Cosmos and set cosmosHolder to its transform.
+            cosmosHolder = new GameObject ("Cosmos").transform;
 
 			//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
 			for(int x = -1; x < columns + 1; x++)
@@ -77,85 +71,30 @@ namespace Completed
 				//Loop along y axis, starting from -1 to place floor or outerwall tiles.
 				for(int y = -1; y < rows + 1; y++)
 				{
-					//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
-					GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
-
-					//Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
-					if(x == -1 || x == columns || y == -1 || y == rows)
-						toInstantiate = outerWallTiles [Random.Range (0, outerWallTiles.Length)];
+                    // TODO: this needs to be changed to use random seed and xxHash
+                    // http://blogs.unity3d.com/2015/01/07/a-primer-on-repeatable-random-numbers/
+                    //Choose a random tile from our array of galaxy tile prefabs and prepare to instantiate it.
+                    GameObject toInstantiate = systemTiles[Random.Range (0, systemTiles.Length)];
 
 					//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
 					GameObject instance =
 						Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
 
-					//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
-					instance.transform.SetParent (boardHolder);
+                    //Set the parent of our newly instantiated object instance to cosmosHolder, this is just organizational to avoid cluttering hierarchy.
+                    instance.transform.SetParent (cosmosHolder);
 				}
 			}
 		}
-
-
-		//RandomPosition returns a random position from our list gridPositions.
-		Vector3 RandomPosition ()
-		{
-			//Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
-			int randomIndex = Random.Range (0, gridPositions.Count);
-
-			//Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
-			Vector3 randomPosition = gridPositions[randomIndex];
-
-			//Remove the entry at randomIndex from the list so that it can't be re-used.
-			gridPositions.RemoveAt (randomIndex);
-
-			//Return the randomly selected Vector3 position.
-			return randomPosition;
-		}
-
-
-		//LayoutObjectAtRandom accepts an array of game objects to choose from along with a minimum and maximum range for the number of objects to create.
-		void LayoutObjectAtRandom (GameObject[] tileArray, int minimum, int maximum)
-		{
-			//Choose a random number of objects to instantiate within the minimum and maximum limits
-			int objectCount = Random.Range (minimum, maximum+1);
-
-			//Instantiate objects until the randomly chosen limit objectCount is reached
-			for(int i = 0; i < objectCount; i++)
-			{
-				//Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
-				Vector3 randomPosition = RandomPosition();
-
-				//Choose a random tile from tileArray and assign it to tileChoice
-				GameObject tileChoice = tileArray[Random.Range (0, tileArray.Length)];
-
-				//Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
-				Instantiate(tileChoice, randomPosition, Quaternion.identity);
-			}
-		}
-
+              
 
 		//SetupScene initializes our level and calls the previous functions to lay out the game board
 		public void SetupScene (int level)
 		{
-			//Creates the outer walls and floor.
-			BoardSetup ();
+            //Creates the grid with galaxies
+            CosmosSetup();
 
 			//Reset our list of gridpositions.
-			InitialiseList ();
-
-			//Instantiate a random number of wall tiles based on minimum and maximum, at randomized positions.
-			LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
-
-			//Instantiate a random number of food tiles based on minimum and maximum, at randomized positions.
-			LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
-
-			//Determine number of enemies based on current level number, based on a logarithmic progression
-			int enemyCount = (int)Mathf.Log(level, 2f);
-
-			//Instantiate a random number of enemies based on minimum and maximum, at randomized positions.
-			LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
-
-			//Instantiate the exit tile in the upper right hand corner of our game board
-			Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0f), Quaternion.identity);
+			InitialiseList ();            
 		}
-	}
+    } // end CosmosManager
 }
