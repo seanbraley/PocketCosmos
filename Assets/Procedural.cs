dd2c65ce25d67bf6bf45d3f8419c128d461d5f07
Susie <hwang.sus@gmail.com>
2016-02-01 18:35:09 -0500

    Basic prefabs for planetary objects and system objects
    Prefabs will call utility scripts.
    To be organized.

---
+++ Assets/Procedural.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Procedural : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+        
+    // TO-DO: Finish.
+    // Static method
+    void SetPlanetaryDetails(PlanetaryBody b, uint seed)
+    {
+
+    }
+
+
+}
