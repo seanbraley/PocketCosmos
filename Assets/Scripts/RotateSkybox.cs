82e4ba34f27ec5b637db4c3ee96c861205a9fe70
Susie <hwang.sus@gmail.com>
2016-02-03 16:52:02 -0500

    Intro screen, scene changes, fancy polishes
    New scenes (start & sector). Start brings up an intro screen. The UI
    should look fine on any of the mobile portrait display modes in Unity.
    Clicking on the start button will load up the sector scene, which gives
    us the sector view (where we left off) with all the stars.
    Menu items may need to be written/updated for Unity 5.
    
    Also added suitably spacey skybox (that rotates) and sounds. Can
    remove/alter later it's nbd.

---
+++ Assets/Scripts/RotateSkybox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Slowly and continually rotates the skybox.
+// Put this on a main camera in the scene.
+public class RotateSkybox : MonoBehaviour {
+
+    float _currentRot = 0;
+    public float _degree = 1;   // degree of rotation
+    	
+	// Update is called once per frame
+	void Update () {
+
+        _currentRot += _degree * Time.deltaTime;
+        _currentRot %= 360;
+        RenderSettings.skybox.SetFloat("_Rotation", _currentRot);
+    }
+            
+}
