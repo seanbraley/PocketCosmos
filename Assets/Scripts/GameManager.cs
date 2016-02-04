e0ab1c3e71f078c217a121f676b30517a2880a6d
Sean Braley <sean.b.braley@gmail.com>
2016-02-04 13:15:46 -0500

    Stars generate with movement (not async)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,23 +4,22 @@ using System.Collections.Generic;
 
 namespace Completed
 {
-    
+
     public class GameManager : MonoBehaviour
     {
 
         public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
 
-        private int offsetX;
-        private int offsetY;
-
         private Vector2 virtualPosition = Vector2.zero;
 
         private int movementCounterX = 0;
         private int movementCounterY = 0;
-        
+
         public GameObject[] starPrefabs;
 
         public List<GameObject> stars = new List<GameObject>();
+
+        public Dictionary<uint, GameObject> starsDic = new Dictionary<uint, GameObject>();
 
         //Awake is always called before any Start functions
         void Awake()
@@ -39,7 +38,7 @@ namespace Completed
 
             //Sets this to not be destroyed when reloading scene
             DontDestroyOnLoad(gameObject);
-            
+
             //Call the InitGame function to initialize the starting level 
             InitGame();
         }
@@ -47,18 +46,15 @@ namespace Completed
         //Initializes the game level.
         void InitGame()
         {
-            offsetX = 0;
-            offsetY = 0;
-
             for (int i = -40; i < 40; i++)
             {
                 for (int j = -40; j < 40; j++)
                 {
-                    uint num = Procedural.PointToNumber(i + offsetX, j + offsetY);
+                    uint num = Procedural.PointToNumber(i, j);
                     BitArray b = new BitArray(new int[] { (int)num });
 
                     bool starExists = Procedural.StarExists(i, j);
-                    
+
                     //bool starExists = (b[5] & b[4]) ^ b[18];
 
                     /* off-grid
@@ -72,75 +68,115 @@ namespace Completed
                     if (starExists)
                     {
                         Debug.Log(string.Format("Star Created at: <{0}, {1}>", i, j));
-                        stars.Add((GameObject)Instantiate(starPrefabs[0], new Vector2(i, j), Quaternion.identity));
+                        starsDic.Add(num, (GameObject)Instantiate(starPrefabs[0], new Vector2(i, j), Quaternion.identity));
                     }
                 }
             }
 
         }
 
+        void TryAddStar(int x, int y)
+        {
+            if (Procedural.StarExists(x, y))
+            {
+                uint num = Procedural.PointToNumber(x, y);
+                starsDic.Add(num, (GameObject)Instantiate(starPrefabs[0], new Vector2(x, y), Quaternion.identity));
+            }
+        }
 
+        void TryRemoveStar(int x, int y)
+        {
+            uint num = Procedural.PointToNumber(x, y);
+            GameObject tmpStar = null;
+            starsDic.TryGetValue(num, out tmpStar);
+            if (tmpStar != null)
+            {
+                starsDic.Remove(num);
+                Destroy(tmpStar, 0f);
+            }
+        }
 
         //Update is called every frame.
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.W))
             {
-                foreach (GameObject s in stars)
+                movementCounterY--;
+                virtualPosition.y++;
+                //if (movementCounterY < -10)
+                {
+                    movementCounterY = 0;
+                    // Generate row of 10 accross top and remove row along bottom
+                    for (int i = -40; i < 40; i++)
+                    {
+                        TryAddStar((int)virtualPosition.x + i, (int)virtualPosition.y + 40);
+                        TryRemoveStar((int)virtualPosition.x + i, (int)virtualPosition.y - 40);
+                    }
+                }
+                foreach (GameObject s in starsDic.Values)
                 {
                     s.transform.Translate(Vector2.down);
                 }
-                movementCounterY--;
-                if (movementCounterY < -10)
-                {
-                    movementCounterY = 0;
-                    // Generate row of 10 accross top and remove row along bottom
-                    
-                }
+
             }
             else if (Input.GetKeyDown(KeyCode.S))
             {
-                foreach (GameObject s in stars)
-                {
-                    s.transform.Translate(Vector2.up);
-                }
                 movementCounterY++;
-                if (movementCounterY > 10)
+                virtualPosition.y--;
+                //if (movementCounterY > 10)
                 {
                     movementCounterY = 0;
-                    // Generate row of 10 accross top and remove row along bottom
-
+                    // Generate row of 10 accross botton and remove row along top
+                    for (int i = -40; i < 40; i++)
+                    {
+                        TryAddStar((int)virtualPosition.x + i, (int)virtualPosition.y - 40);
+                        TryRemoveStar((int)virtualPosition.x + i, (int)virtualPosition.y + 40);
+                    }
                 }
+                foreach (GameObject s in starsDic.Values)
+                {
+                    s.transform.Translate(Vector2.up); // y=1
+                }
+
             }
             else if (Input.GetKeyDown(KeyCode.A))
             {
-                foreach (GameObject s in stars)
-                {
-                    s.transform.Translate(Vector2.right);
-                }
                 movementCounterX++;
-                if (movementCounterX > 10)
+                virtualPosition.x--;
+                //if (movementCounterX > 10)
                 {
                     movementCounterX = 0;
-                    // Generate row of 10 accross top and remove row along bottom
-
+                    // Generate column of 10 accross right and remove column along left
+                    for (int i = -40; i < 40; i++)
+                    {
+                        TryAddStar((int)virtualPosition.x - 40, (int)virtualPosition.y + i);
+                        TryRemoveStar((int)virtualPosition.x + 41, (int)virtualPosition.y + i);
+                    }
+                }
+                foreach (GameObject s in starsDic.Values)
+                {
+                    s.transform.Translate(Vector2.right);
                 }
             }
             else if (Input.GetKeyDown(KeyCode.D))
             {
-                foreach (GameObject s in stars)
-                {
-                    s.transform.Translate(Vector2.left);
-                }
                 movementCounterX--;
-                if (movementCounterY < -10)
+                virtualPosition.x++;
+                //if (movementCounterY < -10)
                 {
                     movementCounterX = 0;
-                    // Generate row of 10 accross top and remove row along bottom
-
+                    // Generate column of 10 accross right and remove column along left
+                    for (int i = -40; i < 40; i++)
+                    {
+                        TryAddStar((int)virtualPosition.x + 40, (int)virtualPosition.y + i);
+                        TryRemoveStar((int)virtualPosition.x - 41, (int)virtualPosition.y + i);
+                    }
+                }
+                foreach (GameObject s in starsDic.Values)
+                {
+                    s.transform.Translate(Vector2.left);
                 }
             }
         }
     }
-
 }
