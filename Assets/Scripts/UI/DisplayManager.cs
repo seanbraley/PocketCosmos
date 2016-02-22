370ffcaf0cdaa4969807edeebf23b373d1524c32
Liam Collins <liamcollins333@gmail.com>
2016-02-22 12:25:21 -0500

    added virtual position onscreen

--- Assets/Scripts/UI/DisplayManager.cs
+++ Assets/Scripts/UI/DisplayManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Completed;
 using UnityEngine.UI;
 using System.Collections;
 
@@ -8,16 +9,19 @@ public class DisplayManager : MonoBehaviour {
     //public Text populationNumber;
    // public Text powerNumber;
     public Text spacebuxNumber;
+    public Text virtualPosition;
 
 	// Use this for initialization
 	void Start () {
         // TO DO literally just copy and paste this for population and power
         spacebuxNumber.text = PlayerData.playdata.Spacebux.ToString();
+        virtualPosition.text = "Position: <" + GameManager.instance.virtualPosition.x + " , " + GameManager.instance.virtualPosition.y + ">";
     }
 	
 	// Update is called once per frame
 	void Update () {
         // TO DO make this less spammy maybe idgaf
         spacebuxNumber.text = PlayerData.playdata.Spacebux.ToString();
+        virtualPosition.text = "Position: <" + GameManager.instance.virtualPosition.x + " , " + GameManager.instance.virtualPosition.y + ">";
     }
 }
