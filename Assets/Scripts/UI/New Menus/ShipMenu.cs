b0aa0dcc091eb25ee000793975a85d96b85ad2d1
Liam Collins <liamcollins333@gmail.com>
2016-03-16 13:27:20 -0500

    Adding in new ship menu design.

---
+++ Assets/Scripts/UI/New Menus/ShipMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShipMenu : MonoBehaviour {
+
+	public static ShipMenu Instance;
+
+	public GameObject ShipMenuItem_Prefab;
+
+	private List<ShipMenuItem> _shipMenuItems;
+
+	private GameObject _layoutGroup;
+
+	// Use this for initialization
+	void Awake () {
+		if (Instance != null && Instance != this) {
+		    Destroy(Instance.gameObject);
+		}
+		Instance = this; //shitty singleton, but anything more ruins everything for whatever reason.
+		                 // I'm not even mad rn.
+
+		_shipMenuItems = new List<ShipMenuItem>();
+		FindLayoutGroup();
+		PopulateShipMenu();
+	}
+
+	void FindLayoutGroup() {
+		_layoutGroup = transform.Find("ScrollViewContainer/ScrollView/LayoutGroup").gameObject;
+		Debug.Log(_layoutGroup != null);
+	}
+
+	void AddShipMenuItem() {
+		GameObject item = Instantiate(ShipMenuItem_Prefab) as GameObject;
+		item.transform.parent = _layoutGroup.transform;
+		item.transform.localScale = new Vector3(1,1,1);
+		_shipMenuItems.Add(item.GetComponent<ShipMenuItem>());
+	}
+
+	void PopulateShipMenu() {
+		// TODO - Actually populate with real ships
+		for (int i = 0; i < Random.Range(3,10); i++) {
+			AddShipMenuItem();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+}
---
+++ Assets/Scripts/UI/New Menus/ShipMenuItem.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShipMenuItem : MonoBehaviour {
+
+	private Text _nameText;
+	public string NameText {
+		get {
+			return _nameText.text;
+		}
+		set {
+			_nameText.text = value;
+		}
+	}
+
+	private Text _statusText;
+	public string StatusText {
+		get {
+			if (_statusText != null)
+				return null;
+			else
+				return _statusText.text;
+		}
+		set {
+			if (_statusText != null)
+				return;
+			else
+				_statusText.text = value;
+		}
+	}
+	public Color StatusColor {
+		get {
+			if (_statusText != null)
+				return new Color(0,0,0,0);
+			else
+				return _statusText.color;
+		}
+		set {
+			if (_statusText != null)
+				return;
+			else
+				_statusText.color = value;
+		}
+	}
+
+	private Image _shipClassImage;
+	public string ShipClass {
+		get {
+			if (_shipClassImage.sprite == ResearchRacerSprite) {
+				return "Research Racer";
+			}
+			else if (_shipClassImage.sprite == ColonyCarrierSprite) {
+				return "Colony Carrier";
+			}
+			else {
+				return null;
+			}
+		}
+		set {
+			if (value == "Research Racer") {
+				_shipClassImage.sprite = ResearchRacerSprite;
+			}
+			if (value == "Colony Carrier") {
+				_shipClassImage.sprite = ColonyCarrierSprite;
+			}
+		}
+	}
+
+	public Sprite ResearchRacerSprite;
+	public Sprite ColonyCarrierSprite;
+
+	void Initialize(ShipInfo info) {
+		_shipClassImage = transform.Find("Image").GetComponent<Image>();
+		_nameText = transform.Find("Name").GetComponent<Text>();
+		_statusText = transform.Find("Status").GetComponent<Text>();
+
+
+		NameText = info.name;
+		ShipClass = info.ship_class;
+
+		if (info.origin_planet != 0 && info.destination_planet != 0) {
+			StatusText = "On Route";
+			StatusColor = Color.yellow;
+		}
+		else if (info.origin_planet != 0 && info.destination_planet == 0) {
+			StatusText = "Arrived";
+			StatusColor = Color.green;
+		}
+		else if (info.origin_planet == 0 && info.destination_planet != 0) {
+			StatusText = "Waiting";
+			StatusColor = Color.red;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
